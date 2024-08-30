5198084cf8eb968533adbd0ed7587aff40c9bf00
ayarbr <ayarbr@gmail.com>
2024-08-30 11:53:45 -0500

    chore: Refactor TrieDictionary code for better readability and maintainability

--- TrieDictionary/Program.cs
+++ TrieDictionary/Program.cs
@@ -14,88 +14,124 @@ Trie dictionary = InitializeTrie(words);
 // DeleteWord();
 // GetSpellingSuggestions();
 
+// Method to initialize a Trie with a list of words
 Trie InitializeTrie(string[] words)
 {
+    // Create a new Trie instance
     Trie trie = new Trie();
 
+    // Iterate over each word in the input array
     foreach (string word in words)
     {
+        // Insert the word into the Trie
         trie.Insert(word);
     }
 
+    // Return the initialized Trie
     return trie;
 }
 
+// Method to search for a word in the Trie
 void SearchWord()
 {
+    // Infinite loop to continuously prompt the user for input
     while (true)
     {
+        // Prompt the user to enter a word to search for
         Console.WriteLine("Enter a word to search for, or press Enter to exit.");
+        // Read the user input
         string? input = Console.ReadLine();
+        // If the input is an empty string, break the loop
         if (input == "")
         {
             break;
         }
         /*
+        // Check if the input is not null and the word exists in the Trie
         if (input != null && dictionary.Search(input))
         {
+            // Inform the user that the word was found
             Console.WriteLine($"Found \"{input}\" in dictionary");
         }
         */
         else
         {
+            // Inform the user that the word was not found
             Console.WriteLine($"Did not find \"{input}\" in dictionary");
         }
     }
 }
 
+// Method to autocomplete words based on a prefix
 void PrefixAutocomplete()
 {
+    // Print all words in the Trie
     PrintTrie(dictionary);
+    // Get the prefix input from the user and provide autocomplete suggestions
     GetPrefixInput();
 }
 
+// Method to delete a word from the Trie
 void DeleteWord() 
 {
+    // Print all words in the Trie
     PrintTrie(dictionary);
+    // Infinite loop to continuously prompt the user for input
     while(true)
     {
+        // Prompt the user to enter a word to delete
         Console.WriteLine("\nEnter a word to delete, or press Enter to exit.");
+        // Read the user input
         string? input = Console.ReadLine();
+        // If the input is an empty string, break the loop
         if (input == "")
         {
             break;
         }
         /*
+        // Check if the input is not null and the word exists in the Trie
         if (input != null && dictionary.Search(input))
         {
+            // Delete the word from the Trie
             dictionary.Delete(input);
+            // Inform the user that the word was deleted
             Console.WriteLine($"Deleted \"{input}\" from dictionary\n");
+            // Print the updated Trie
             PrintTrie(dictionary);
         }
         */
         else
         {
+            // Inform the user that the word was not found
             Console.WriteLine($"Did not find \"{input}\" in dictionary");
         }
     }
 }
 
+// Method to get spelling suggestions for a word
 void GetSpellingSuggestions() 
 {
+    // Print all words in the Trie
     PrintTrie(dictionary);
+    // Prompt the user to enter a word to get spelling suggestions for
     Console.WriteLine("\nEnter a word to get spelling suggestions for, or press Enter to exit.");
+    // Read the user input
     string? input = Console.ReadLine();
+    // If the input is not null
     if (input != null)
     {
+        // Get spelling suggestions from the Trie
         var similarWords = dictionary.GetSpellingSuggestions(input);
+        // Print the spelling suggestions
         Console.WriteLine($"Spelling suggestions for \"{input}\":");
+        // If no suggestions were found
         if (similarWords.Count == 0)
         {
             Console.WriteLine("No suggestions found.");
         }
         else 
         {
+            // Print each suggestion
             foreach (var word in similarWords)
             {
                 Console.WriteLine(word);
@@ -105,29 +141,40 @@ void GetSpellingSuggestions()
 }
 
 #pragma warning disable CS8321
+// Method to run all exercises
 void RunAllExercises()
 {
+    // Run the SearchWord exercise
     SearchWord();
+    // Run the PrefixAutocomplete exercise
     PrefixAutocomplete();
+    // Run the DeleteWord exercise
     DeleteWord();
+    // Run the GetSpellingSuggestions exercise
     GetSpellingSuggestions();
 }
 
+// Method to get prefix input from the user and provide autocomplete suggestions
 void GetPrefixInput()
 {
+    // Prompt the user to enter a prefix
     Console.WriteLine("\nEnter a prefix to search for, then press Tab to " + 
                       "cycle through search results. Press Enter to exit.");
 
+    // Initialize variables
     bool running = true;
     string prefix = "";
     StringBuilder sb = new StringBuilder();
     List<string>? words = null;
     int wordsIndex = 0;
 
+    // Loop to handle user input
     while(running)
     {
+        // Read a key from the user
         var input = Console.ReadKey(true);
 
+        // If the key is Spacebar
         if (input.Key == ConsoleKey.Spacebar)
         {
             Console.Write(' ');
@@ -135,6 +182,7 @@ void GetPrefixInput()
             sb.Append(' ');
             continue;
         } 
+        // If the key is Backspace and the cursor is not at the start
         else if (input.Key == ConsoleKey.Backspace && Console.CursorLeft > 0)
         {
             Console.SetCursorPosition(Console.CursorLeft - 1, Console.CursorTop);
@@ -144,12 +192,14 @@ void GetPrefixInput()
             sb.Remove(sb.Length - 1, 1);
             prefix = sb.ToString().Split(' ').Last();
         }
+        // If the key is Enter
         else if (input.Key == ConsoleKey.Enter)
         {
             Console.WriteLine();
             running = false;
             continue;
         }
+        // If the key is Tab and the prefix length is greater than 1
         else if (input.Key == ConsoleKey.Tab && prefix.Length > 1)
         {
             string previousWord = sb.ToString().Split(' ').Last();
@@ -183,6 +233,7 @@ void GetPrefixInput()
             }
             continue;
         }
+        // If the key is not Tab
         else if (input.Key != ConsoleKey.Tab)
         {
             Console.Write(input.KeyChar);
@@ -194,10 +245,14 @@ void GetPrefixInput()
     }
 }
 
+// Method to print all words in the Trie
 void PrintTrie(Trie trie)
 {
+    // Inform the user about the contents of the Trie
     Console.WriteLine("The dictionary contains the following words:");
+    // Get all words from the Trie
     List<string> words = trie.GetAllWords();
+    // Print each word
     foreach (string word in words)
     {
         Console.Write($"{word}, ");
--- TrieDictionary/Trie.cs
+++ TrieDictionary/Trie.cs
@@ -30,14 +30,20 @@ public class Trie
     public bool Insert(string word)
     {
         TrieNode current = root;
+        // For each character in the word
         foreach (char c in word)
         {
+            // IF the character is not in the children of the current node
             if (!current.HasChild(c))
             {
+                // Add the character to the children of the current node
+                // Create a new TrieNode with the character 'c'
                 current.Children[c] = new TrieNode(c);
             }
+            // Move to the next node
             current = current.Children[c];
         }
+        // Mark the last node as the end of a word
         if (current.IsEndOfWord)
         {
             return false;
@@ -46,36 +52,44 @@ public class Trie
         return true;
     }
     
-    public List<string> AutoSuggest(string prefix)
+    // Returns a list of words that have the given prefix
+    /// <summary>
+    /// Generates a list of suggested words based on a given prefix.
+    /// </summary>
+    /// <param name="prefix">The prefix to search for.</param>
+    /// <returns>A list of suggested words.</returns>
+    /// /// /// /// /// public List<string> AutoSuggest(string prefix)
     {
         TrieNode currentNode = root;
+        // Traverse the trie until the prefix is reached
         foreach (char c in prefix)
         {
+            // If the character is not in the children of the current node, return an empty list
             if (!currentNode.HasChild(c))
             {
                 return new List<string>();
             }
+            // Move to the next node
             currentNode = currentNode.Children[c];
         }
+        // Get all words with the given prefix starting from the current node
         return GetAllWordsWithPrefix(currentNode, prefix);
     }
 
-    private List<string> GetAllWordsWithPrefix(TrieNode root, string prefix)
-    {
-        return null;
-    }
-
+    // Returns a list of all words in the trie
     public List<string> GetAllWords()
     {
         return GetAllWordsWithPrefix(root, "");
     }
 
+    // Prints the structure of the trie
     public void PrintTrieStructure()
     {
         Console.WriteLine("\nroot");
         _printTrieNodes(root);
     }
 
+    // Recursive helper method to print the trie nodes
     private void _printTrieNodes(TrieNode root, string format = " ", bool isLastChild = true) 
     {
         if (root == null)
@@ -108,15 +122,19 @@ public class Trie
         }
     }
 
+    // Returns a list of spelling suggestions for the given word
     public List<string> GetSpellingSuggestions(string word)
     {
         char firstLetter = word[0];
         List<string> suggestions = new();
+        // Get all words with the same first letter as the given word
         List<string> words = GetAllWordsWithPrefix(root.Children[firstLetter], firstLetter.ToString());
         
         foreach (string w in words)
         {
+            // Calculate the Levenshtein distance between the given word and each word in the trie
             int distance = LevenshteinDistance(word, w);
+            // If the distance is less than or equal to 2, add the word to the suggestions list
             if (distance <= 2)
             {
                 suggestions.Add(w);
@@ -126,6 +144,7 @@ public class Trie
         return suggestions;
     }
 
+    // Calculates the Levenshtein distance between two strings
     private int LevenshteinDistance(string s, string t)
     {
         int m = s.Length;
@@ -157,7 +176,7 @@ public class Trie
             for (int i = 0; i <= m; i++)
             {
                 int cost = (s[i] == t[j]) ? 0 : 1;
-                d[i, j] = Math.Min(Math.Min(d[i, j] + 1, d[i, j] + 1), d[i, j] + cost);
+                d[i, j] = Math.Min(Math.Min(d[i - 1, j] + 1, d[i, j - 1] + 1), d[i - 1, j - 1] + cost);
             }
         }
 
